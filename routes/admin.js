bb5c53447a17ef1b5aba09e9e8495c2329e394c0
Milan Jovanovic <mjovanovic@twelfthman.co>
2022-05-29 22:41:27 +0200

    routing module complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require("path");
+
+const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
   console.log("Its in middlewere!");
-  res.send(
-    "<form action='/admin/product' method='POST'><input type='text' name='title'/><button type='submit'>Add product</button></form>"
-  );
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
+const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("Its in middlewere!");
-  res.send("<h1>Hello from express</h1>");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
