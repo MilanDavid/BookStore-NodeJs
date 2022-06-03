ba1e0093e21234d096d7e699c90f07c44797660e
Milan Jovanovic <mjovanovic@twelfthman.co>
2022-06-03 11:09:04 +0200

    pug templating

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const path = require("path");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page not Found!" });
 });
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -5,14 +5,17 @@ const rootDir = require("../helpers/path");
 
 const router = express.Router();
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
   console.log("Its in middlewere!");
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("add-product", { pageTitle: "Add Product", path: "/admin/add-product" });
 });
 
-router.post("/product", (req, res, next) => {
-  console.log("[EXPRESS BODY]: ", req.body);
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
