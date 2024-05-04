f55ade438f96cebd32e8696800ab8d485b30a45f
Milan Jovanovic <mjovanovic@twelfthman.co>
2024-05-04 14:56:57 +0200

    chore: Update admin controller to support editing products

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,43 @@
 const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  console.log("Its in middlewere!");
-  res.render("admin/add-product", {
+  res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
-    activeAddProduct: true,
-    formCss: true,
-    productCSS: true,
+    editing: false
+  });
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
   });
 };
 
 exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, description, price);
+  product.save();
+  res.redirect("/");
+};
+
+exports.postEditProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
