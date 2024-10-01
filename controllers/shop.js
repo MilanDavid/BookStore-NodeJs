c7c2a1820a0e6d14b5b2768a81a7bc053229090c
Milan Jovanovic <mjovanovic@twelfthman.co>
2024-10-01 14:52:17 +0200

    Remove console.log statement in getOrders controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -141,7 +141,6 @@ exports.getOrders = (req, res, next) => {
   req.user
     .getOrders({ include: ["products"] })
     .then((orders) => {
-      console.log("[ORDERS]: ", orders);
       res.render("shop/orders", {
         path: "/orders",
         pageTitle: "Your Orders",
